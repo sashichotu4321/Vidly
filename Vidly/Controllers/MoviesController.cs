9c0d4ab4692a6ddd0b290464306061ec1e4a2b1c
sashi <chotusashi4321@gmail.com>
2016-10-09 20:24:11 +0530

    Uploading on Git

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            //Initializing Model
+            var movie = new Movie() { Name = "Shrek!" };
+
+            //creating list of customers
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Customer1"},
+                new Customer {Name = "Customer2"}
+            };
+
+            //Initializing View Model
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+
+            //ViewData["Movie"] = movie; - do not use
+            //ViewBag.Movie = movie;
+
+            //return View(movie);
+
+            //return Content("Hello World!");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home", new {page = 1, sortBy = "name"});
+
+        }
+
+        /* public ActionResult Edit(int id)
+        {
+            return Content("id =" + id);
+        }
+
+        public ActionResult Index(int? pageindex, string sortBy)
+        {
+            if (!pageindex.HasValue)
+                pageindex = 1;
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+            return Content(String.Format("pageindex={0}&sortBy={1}", pageindex, sortBy));
+        } */
+
+
+
+        [Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+            //return View();
+        }
+    }
+}
